2fbfbb3de0737d32500004588865eddf863f46f8
Ragul8775 <rb7439@srmist.edu.in>
2024-02-26 14:01:27 +0530

    first contract'

---
+++ Basics/Counter.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.0;
+
+contract Counter {
+    uint count;
+    constructor(){
+        count =0;
+    }
+    function getCount() public view returns (uint){
+        return count;
+    }
+    function increaseCount() public {
+        count = count+1;
+    }
+}
